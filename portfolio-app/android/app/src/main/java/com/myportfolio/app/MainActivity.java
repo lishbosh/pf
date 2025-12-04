03c5a396228a88049421a19ec4a27fe6adfca19a
lishbosh <siyanoav917@gmail.com>
2025-12-04 18:29:56 +0530

    Fix SplashScreen implementation to properly use AndroidX SplashScreen library

--- portfolio-app/android/app/src/main/java/com/myportfolio/app/MainActivity.java
+++ portfolio-app/android/app/src/main/java/com/myportfolio/app/MainActivity.java
@@ -1,5 +1,11 @@
 package com.myportfolio.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

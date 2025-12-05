c5c01e4d115b6103092df8805ab63470b684cb6b
lishbosh <siyanoav917@gmail.com>
2025-12-05 13:59:23 +0530

    Simplify capacitor config to fix app crash

--- portfolio-app/capacitor.config.ts
+++ portfolio-app/capacitor.config.ts
@@ -3,17 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.myportfolio.app',
   appName: 'portfolio-app',
-  webDir: 'out',
-  server: {
-    androidScheme: 'https',
-    hostname: 'localhost',
-    iosScheme: 'https',
-    cleartext: false,
-    url: 'index.html'
-  },
-  android: {
-    webContentsDebuggingEnabled: true
-  }
+  webDir: 'out'
 };
 
 export default config;

e9f8fc49ea2af8c333618bcccd1b845c3bcb4b2b
lishbosh <siyanoav917@gmail.com>
2025-12-06 13:45:52 +0530

    Remove unused loading.tsx file

--- portfolio-app/src/app/print/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div style={{ padding: '1rem', textAlign: 'center' }}>
-      <p style={{ color: '#374151' }}>Loading print preview...</p>
-    </div>
-  );
-}
